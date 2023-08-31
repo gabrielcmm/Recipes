be9907fc6711d1fb4e8e792f4e41cc6323348e27
Gabriel <gabrielcust@gmail.com>
2023-08-31 08:18:45 -0300

    instruction and videos

--- src/components/RecipeDetailsPattern/RecipeDetailsInstruction.tsx
+++ src/components/RecipeDetailsPattern/RecipeDetailsInstruction.tsx
@@ -7,24 +7,25 @@ function RecipeDetailsInstruction(
   { instruction, videoUrl = undefined }: RecipeDetailsInstructionsProps,
 ) {
   return (
-    <div>
-      <h2>Instructions</h2>
+    <div className="p-2">
+      <h2 className="text-2xl mb-3">Instructions</h2>
       <p
         data-testid="instructions"
-        className="whitespace-break-spaces leading-2"
+        className="whitespace-break-spaces leading-2 mb-3"
       >
         {instruction}
       </p>
 
       {videoUrl && (
-        <>
-          <h2>Video</h2>
+        <div>
+          <h2 className="text-2xl mb-3">Video</h2>
           <iframe
+            className="w-full aspect-video rounded-xl"
             data-testid="video"
             title="video"
             src={ videoUrl }
           />
-        </>
+        </div>
       )}
     </div>
   );
